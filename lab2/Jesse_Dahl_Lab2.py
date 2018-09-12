b6d658ecc02ff59047a1a8b72a00e14ca53b0ca0
jesse-dahl <40815531+jesse-dahl@users.noreply.github.com>
2018-09-11 23:54:50 -0600

    Add files via upload
    The finished lab 2 file with updated comments

--- lab2/Jesse_Dahl_Lab2.py
+++ lab2/Jesse_Dahl_Lab2.py
@@ -19,7 +19,6 @@ square.up()
 square.goto(-205, 205)
 square.write("Change Color")
 
-#New code that creates the new square with "Move Mouse" written over the top of it
 square.up()
 square.goto(200,200)
 square.down()
@@ -31,27 +30,25 @@ for i in range(4):
 square.up()
 square.goto(195,205)
 square.write("Move Mouse")
-#New code for square ends
-
-#New function that moves the mouse to a random point on the screen
         
 pencil = turtle.Turtle()
 pencil.shape("circle")
 
 def drawing_controls(x,y):
-	if (-200 <= x <= -150) and (150 <= y <= 200):
-        	red = random.random()
-        	green = random.random()
-        	blue = random.random()
-        	pencil.color(red, green, blue)
-	elif (200 <= x <= 250) and (150 <= y <= 200):
-		pencil.up()
-		x_chor = random.randint(-350,350)
-		y_chor = random.randint(-350,350)
-		pencil.goto(x_chor, y_chor)
-		pencil.down()
+    if (-200 <= x <= -150) and (150 <= y <= 200):
+        red = random.random()
+        green = random.random()
+        blue = random.random()
+        pencil.color(red, green, blue)
+    elif (200 <= x <= 250) and (150 <= y <= 200):
+        pencil.up()
+        x_chor = random.randint(-300,300)
+        y_chor = random.randint(-300,300)
+        pencil.goto(x_chor, y_chor)
+        pencil.down()
+
 
 window.onclick(drawing_controls)
-pencil.onrelease(pencil.goto)
 
-turtle.mainloop()
+pencil.onrelease(pencil.goto)
+        
