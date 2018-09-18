a97d699c5195fd93724afade5c2b11a9c2a32581
jesse-dahl <40815531+jesse-dahl@users.noreply.github.com>
2018-09-17 21:19:49 -0600

    Create lab3.py

---
+++ lab3/lab3.py
@@ -0,0 +1,69 @@
+#THIS IS THE ORIGINAL LAB3.PY FILE MENTIONED IN THE ASSIGNMENT INSTRUCTIONS OF README.MD
+
+# --------------------------------------
+# CSCI 127, Lab 3
+# September 18, 2018
+# Your Name
+# --------------------------------------
+
+def count_vowels(sentence):
+    pass
+
+def count_vowels_iterative(sentence):
+    pass
+# --------------------------------------
+# CSCI 127, Lab 3
+# September 18, 2018
+# Your Name
+# --------------------------------------
+
+def count_vowels(sentence):
+    pass
+
+def count_vowels_iterative(sentence):
+    pass
+
+def remove_iterative(sentence):
+    return ""
+
+
+# --------------------------------------
+
+def main():
+    answer = "yes"
+    while (answer == "yes") or (answer == "y"):
+        sentence = input("Please enter a sentence to evaluate: ")
+        sentence = sentence.lower()     # convert to lowercase
+        print()
+        print("Number of vowels using count     =", count_vowels(sentence))
+        print("Number of vowels using iteration =", count_vowels_iterative(sentence))
+        print("Using iteration = |" + remove_iterative(sentence) + "|")
+        print()
+        answer = input("Would you like to continue: ").lower()
+        print()
+
+# --------------------------------------
+
+main()
+def remove_iterative(sentence):
+    return ""
+
+
+# --------------------------------------
+
+def main():
+    answer = "yes"
+    while (answer == "yes") or (answer == "y"):
+        sentence = input("Please enter a sentence to evaluate: ")
+        sentence = sentence.lower()     # convert to lowercase
+        print()
+        print("Number of vowels using count     =", count_vowels(sentence))
+        print("Number of vowels using iteration =", count_vowels_iterative(sentence))
+        print("Using iteration = |" + remove_iterative(sentence) + "|")
+        print()
+        answer = input("Would you like to continue: ").lower()
+        print()
+
+# --------------------------------------
+
+main()
